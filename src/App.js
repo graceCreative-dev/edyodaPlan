f692a4b32df24531065cf88852ee203bf9d0f98b
Sumit-dev <129378436+graceCreative-dev@users.noreply.github.com>
2023-07-16 13:48:35 +0530

    EdYoda Payment Page - React Coding Assingment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from './Component/Main';
+import NavBar from './Component/NavBar';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Main/>
+    </>
+  );
+}
+
+export default App;
